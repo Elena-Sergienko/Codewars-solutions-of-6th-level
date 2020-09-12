1e54ffad3d34f1ea7fc95e2a706758a2ff07aae1
Elena-Sergienko <elena.serg.us@gmail.com>
2020-09-12 11:48:11 -0700

    Add solution-2 (Unique In Order.js)

---
+++ Sum of Digits.js
@@ -0,0 +1,25 @@
+// 6 kyu
+// Sum of Digits / Digital Root
+// https://www.codewars.com/kata/541c8630095125aba6000c00/train/javascript
+// Digital root is the recursive sum of all the digits in a number.
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+// Examples
+// 16  -->  1 + 6 = 7
+// 942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11
+
+function sumOfDigits(n) {
+    let sum = 0;
+    for (let digit of n.toString()) {
+        sum += +digit;
+    }
+    return sum;
+}
+
+function digital_root(n) {
+    while (n > 9) {
+        n = sumOfDigits(n);
+    }
+    return n;
+}
